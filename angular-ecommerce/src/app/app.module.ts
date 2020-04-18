9fc12e745a20c452bd41ca528ee59be3138a6747
ltly602634744 <yan.liyan1204@gmail.com>
2020-04-18 12:21:23 -0400

    update angular component to subscribe to data from angular service

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './component/product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from '@angular/common/http'
 import { ProductService } from './service/product.service';
 @NgModule({
