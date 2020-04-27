05d497afba4ed8856f9429e5caf0246b459ddd5f
ltly602634744 <yan.liyan1204@gmail.com>
2020-04-27 17:06:58 -0400

    pagination for search by keyword

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -25,13 +25,13 @@ export class ProductService {
   }
 
   getProductListPaginate(thePage: number,
-                         thePageSize: number,
-                         theCategoryId: number): Observable<GetResponseProducts> {
+    thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
 
     // need to build URL based on category id, page and size 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-                    + `&page=${thePage}&size=${thePageSize}`;
-   
+      + `&page=${thePage}&size=${thePageSize}`;
+
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
@@ -58,11 +58,22 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(thePage: number,
+    thePageSize: number,
+    theKeyword: string): Observable<GetResponseProducts> {
+
+    // need to build URL based on keyword, page and size 
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
 
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient
-               .get<GetResponseProducts>(searchUrl)
-               .pipe(map(response => response._embedded.products));
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map(response => response._embedded.products));
   }
 }
 
